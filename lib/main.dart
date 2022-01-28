2c03168c05fda25c9710de030dcf6cdb79c3b3e4
Yusraalabbar <ysraalabbar@gmail.com>
2022-01-28 21:17:54 +0000

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
-import 'package:electricity/view/mainPage/consumptionMain/value_monitor.dart';
 import 'package:electricity/view/mainPage/startSreen/routes.dart';
+//import 'package:electricity/view/mainPage/consumptionMain/value_monitor.dart';
+//import 'package:electricity/view/mainPage/startSreen/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'model/pref.dart';
 
 var select = 1;
 bool login = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
 
   var user = FirebaseAuth.instance.currentUser;
